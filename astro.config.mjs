03dbde6db69c2cbadb52e2a4dfbb9ec62c0e2329
ivandsj2 <ivandsj2000@gmail.com>
2026-04-27 19:59:59 +0200

    Final4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://ivandsj2.github.io/',
-  base: '/ivansiles.github.io'
+  site: 'https://ivandsj2.github.io/'
 });
